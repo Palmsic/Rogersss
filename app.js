217e856031dcad62dcdb705a14f92cab85793992
Palmsic <143403881+Palmsic@users.noreply.github.com>
2023-10-09 05:55:11 -0600

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_j6ch2fz", "template_j89swfg", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
